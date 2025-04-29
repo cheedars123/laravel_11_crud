d4d9da03bc072e1b9743d5d67c8804a780b2ba17
pionayyy11 <fionasamanthae@gmail.com>
2025-04-29 22:33:49 +0800

    login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthorizationController;
+
+
+
 Route::get('/', function () {
  return view('welcome');
 });
 Route::resource('products', ProductController::class);
+
+
+// Auth Routes
+Route::get('/register', [AuthorizationController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthorizationController::class, 'register']);
+
+Route::get('/login', [AuthorizationController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthorizationController::class, 'login']);
+
+Route::get('/logout', [AuthorizationController::class, 'logout'])->name('logout');
+
+// Protected route
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth');
