dea489419879a4815fd2c2b9ec385ea44e21fa92
cheed <cheedkyut08@gmail.com>
2025-05-16 23:44:17 +0800

    sulod

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
+
 Route::get('/', function () {
- return view('welcome');
+    return view('welcome');
 });
+
 Route::resource('products', ProductController::class);
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store'])->name('register.post');
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store'])->name('login.post');
